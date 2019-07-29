50fe871a44121a1fa65e01fdded0421606e27642
Yarin <15931306+TheYarin@users.noreply.github.com>
2019-07-29 22:30:59 +0300

    added Duration column to the sqlite

--- OutageRecordsRepository.py
+++ OutageRecordsRepository.py
@@ -1,4 +1,5 @@
 import sqlite3
+import datetime
 
 class OutageRecordsRepository(object):
     def __init__(self, dbFileName):
@@ -8,23 +9,25 @@ class OutageRecordsRepository(object):
     def createNewOutage(self, outageStartDateTime, outageEndDateTime):
         startTimestamp = OutageRecordsRepository._datetimeToString(outageStartDateTime)
         endTimestamp = OutageRecordsRepository._datetimeToString(outageEndDateTime)
+        duration = OutageRecordsRepository.toNiceTimeDelta(outageStartDateTime, outageEndDateTime)
         
-        INSERT_OUTAGE_END_SQL = "INSERT INTO Outages(Start,End) VALUES(?,?)"
-
-        self._executeAndCommit(INSERT_OUTAGE_END_SQL, parameters=(startTimestamp, endTimestamp))
+        INSERT_OUTAGE_END_SQL = "INSERT INTO Outages(Start,End, Duration) VALUES(?,?,?)"
+        self._executeAndCommit(INSERT_OUTAGE_END_SQL, parameters=(startTimestamp, endTimestamp, duration))
 
     def updateOutageEndTimestamp(self, outageStartDateTime, newOutageEndDateTime):
         startTimestamp = OutageRecordsRepository._datetimeToString(outageStartDateTime)
         newEndTimestamp = OutageRecordsRepository._datetimeToString(newOutageEndDateTime)
-        
-        UPDATE_OUTAGE_END_SQL = "UPDATE Outages SET End = ? WHERE Start = ?"
+        duration = OutageRecordsRepository.toNiceTimeDelta(outageStartDateTime, newOutageEndDateTime)
 
-        self._executeAndCommit(UPDATE_OUTAGE_END_SQL, parameters=(newEndTimestamp, startTimestamp))
+        UPDATE_OUTAGE_END_SQL = "UPDATE Outages SET End = ?, Duration = ? WHERE Start = ?"
+
+        self._executeAndCommit(UPDATE_OUTAGE_END_SQL, parameters=(newEndTimestamp, duration, startTimestamp))
 
     def _createOutagesTable(self):
         CREATE_OUTAGES_TABLE_SQL = """CREATE TABLE IF NOT EXISTS Outages (
                                           Start text PRIMARY KEY,
-                                          End text
+                                          End text,
+                                          Duration text
                                       );
                                       """
         
@@ -43,3 +46,16 @@ class OutageRecordsRepository(object):
     @staticmethod
     def _datetimeToString(datetime):
         return datetime.strftime('%Y-%m-%d %H:%M:%S.%f')
+
+    @staticmethod
+    def toNiceTimeDelta(startDatetime, endDatetime):
+        delta = endDatetime - startDatetime
+        if (delta.days > 0):
+            out = str(delta).replace(" days, ", ":")
+        else:
+            out = "0:" + str(delta)
+        outAr = out.split(':')
+        outAr = ["%02d" % (int(float(x))) for x in outAr]
+        out   = ":".join(outAr)
+        
+        return out
